03a20700835ea586a4ceed2c2dbeb714f85f3367
kevokl <lacartax@gmail.com>
2023-03-23 11:35:41 -0600

    Version final ahora si

--- js/global.js
+++ js/global.js
@@ -14,19 +14,19 @@ const renderNavBar = () => {
     if (loggedIn){
         linkList.innerHTML = `
                 <li class="nav-item ms-2">
-                    <a class="nav-link twhite text-fluid" href="./pages/products.html">Comprar</a>
+                    <a class="nav-link twhite text-fluid" href="../pages/products.html">Comprar</a>
                 </li>
                 <li class="nav-item ms-2">
                     <a class="nav-link twhite text-fluid" href="#">Vender</a>
                 </li>
                 <li class="nav-item ms-2 dropdown ">
-                    <a class="nav-link dropdown-toggle twhite text-fluid" href="./pages/customer.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle twhite text-fluid" href="../pages/customer.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Perfil
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item twhite" href="./pages/customer.html">Mi Perfil</a></li>
+                    <li><a class="dropdown-item twhite" href="../pages/customer.html">Mi Perfil</a></li>
                         <li><a class="dropdown-item twhite" href="#">Compras</a></li>
-                        <li><a class="dropdown-item twhite" href="./pages/vendedor.html">Ventas</a></li>
+                        <li><a class="dropdown-item twhite" href="../pages/vendedor.html">Ventas</a></li>
                         <li><hr class="dropdown-divider twhite"></li>
                         <li><a class="dropdown-item twhite" href="#" id="logOut">Cerrar Sesión</a></li>
                     </ul>    
@@ -43,10 +43,10 @@ const renderNavBar = () => {
     }else{
         linkList.innerHTML = `
                 <li class="nav-item ms-2">
-                    <a class="nav-link twhite text-fluid" href="./pages/products.html">Comprar</a>
+                    <a class="nav-link twhite text-fluid" href="../pages/products.html">Comprar</a>
                 </li>
                 <li class="nav-item ms-2">
-                    <a class="nav-link twhite text-fluid text-nowrap" href="./pages/login.html" id="logIn">Inicia Sesión</a>
+                    <a class="nav-link twhite text-fluid text-nowrap" href="../pages/login.html" id="logIn">Inicia Sesión</a>
                 </li>
             `;
             let logIn = document.getElementById('logIn');
